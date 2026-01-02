6ab7209fa4452e737bddb6f2041bfc36cdcc08d1
Omarques <otavio.marques20@hotmail.com>
2026-01-01 22:19:20 -0300

    Corrigi icone

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'leaflet/dist/leaflet.css'
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-createApp(App).mount('#app')
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+createApp(App).mount("#app");
