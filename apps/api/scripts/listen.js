ebe638f6f92ffcc3eb787b3d60fb2917b32d1ccf
Omarques <otavio.marques20@hotmail.com>
2026-01-01 22:09:20 -0300

    Corrige workflow webapp azure

--- apps/api/scripts/listen.js
+++ apps/api/scripts/listen.js
@@ -1,6 +1,11 @@
 const { io } = require("socket.io-client");
 
-const socket = io("http://localhost:3000", { path: "/socket.io" });
+const BASE = "https://ca-driver-poc.gentlehill-44781624.brazilsouth.azurecontainerapps.io";
+
+const socket = io(BASE, {
+  path: "/socket.io",
+  transports: ["websocket"],
+});
 
 socket.on("connect", () => {
   console.log("connected", socket.id);
@@ -10,3 +15,4 @@ socket.on("connect", () => {
 socket.on("joined", (m) => console.log("joined", m));
 socket.on("location", (m) => console.log("location", m));
 socket.on("disconnect", () => console.log("disconnected"));
+socket.on("error", (e) => console.log("error", e));
